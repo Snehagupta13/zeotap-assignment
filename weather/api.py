9cb5cd0fd93eb43658c92ececede55c8cac3f9fb
Sneha Gupta <105127967+Snehagupta13@users.noreply.github.com>
2024-10-20 10:50:48 +0530

    Update api.py

--- weather/api.py
+++ weather/api.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-# API_KEY = os.getenv("7d480126d5a71e371162f13c5ee233f0")  # Set this in your environment
+
 
 def fetch_weather_data(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
